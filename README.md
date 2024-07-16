b07495d33ff459f2c99bf78afb2b26e2f5c7352d
khaleel <khaleelsyed@users.noreply.github.com>
2024-07-16 21:57:19 +0100

    update everything

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # dotfiles
+
+## Usage
+
+**Clone the repo**:
+
+```bash
+git clone https://github.com/khaleelsyed/dotfiles.git
+```
+
+**Install the dot files**:
+
+_**NOTE**: This requires sudo access (installs `stow`)_
+
+```bash
+cd dotfiles
+bash install.sh
+```
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Majority of this script has been taken from: https://github.com/okwilkins/.dotfiles
+
+sudo apt update
+
+# Install stow
+sudo apt install stow -y
+
+cd configs
+
+for dir in */; do
+    echo "Initiating stow for directory: $dir"
+    stow -D $dir
+    stow $dir -t ~
+done
+
+# If running WSL, VSCode needs to be in a seperate location
+if grep -qi Microsoft /proc/version && [[ -f $HOME/.config/Code/User/settings.json ]]; then
+  echo "Running WSL, symlinking VSCode settings"
+  ln -sf $HOME/.config/Code/User/settings.json $HOME/.vscode-server/data/Machine/settings.json
+fi
+
+# If running VSCode install extensions
+if which code >/dev/null; then
+  echo "Installing VSCode extensions"
+  cat vscode/extensions.txt | xargs -L 1 code --install-extension
+else
+  echo "VSCode command does not exist, not installing extensions"
+fi
