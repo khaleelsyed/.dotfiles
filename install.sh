14a1ee6bc94086566fa6008a0ecddc11bd27d307
khaleel <khaleelsyed@users.noreply.github.com>
2024-07-17 01:43:40 +0100

    update again

--- install.sh
+++ install.sh
@@ -34,6 +34,23 @@ if ! curl -fsSL https://fnm.vercel.app/install | bash; then
   exit 1;
 fi
 
-bash node_pnpm_install.sh
+echo "Running env var stuff" # DEV
+FNM_PATH="/home/khaleel/.local/share/fnm"
+export PATH="$FNM_PATH:$PATH"
+eval "`fnm env`"
+
+# Download and install Node.js (latest LTS version)
+if ! fnm install --lts; then
+  echo "Failed to install Node.js"
+  exit 1;
+fi
+
+# Install pnpm
+if curl -fsSL https://get.pnpm.io/install.sh | sh -; then
+  exit 0;
+else
+  echo "Failed to install pnpm"
+  exit 1;
+fi
 
 exit 0;
